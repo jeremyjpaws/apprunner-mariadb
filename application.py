62d9dc546d14ef13af8259261b9ac2c298de5848
root <root@ip-172-64-1-226.ec2.internal>
2024-02-16 21:07:55 +0000

    3

--- application.py
+++ application.py
@@ -62,7 +62,7 @@ app.logger.debug(f"Finished start. Successfully connected to your mariadb. Table
 
 @app.route("/")
 def index():  
-  return "Hello World! 2"
+  return "Hello World! 3"
 
 @app.route("/hello")
 def hello():
